06c5c0ed0e60fba029cdc8fbf3fbaac56674bb2d
KinshinyerR <kinshinyer02@gmail.com>
2021-08-04 19:51:22 -0500

    UserForm Style

--- src/pages/Users/UserForm.jsx
+++ src/pages/Users/UserForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Plantilla from "../../componentes/Plantilla/Plantilla";
+
 import { registerUser } from "../../lib/services/users/users.service";
 
 const UserForm = () => {
@@ -24,62 +24,92 @@ const UserForm = () => {
 
   return (
     <>
-      <Plantilla title="Usuario" />
-      <form onSubmit={handleOnSubmit}>
-        <div>
-          <label htmlFor="nombre">Nombre</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            id="nombre"
-            name="nombre"
-            value={nombre}
-          />
+      <form
+        onSubmit={handleOnSubmit}
+        className="border border-warning rounded container p-5 w-50"
+      >
+        <h1 className="mb-5">Registrar Usuario</h1>
+        <div className="ps-5 ms-5">
+          <div className="mb-3 row">
+            <label htmlFor="nombre" className="col-sm-2 col-form-label">
+              Nombre
+            </label>
+            <div className="col-sm-10">
+              <input
+                onChange={handleOnChange}
+                type="text"
+                id="nombre"
+                name="nombre"
+                value={nombre}
+                className="w-50"
+              />
+            </div>
+          </div>
+          <div className="mb-3 row">
+            <label htmlFor="apellido" className="col-sm-2 col-form-label">
+              Apellido
+            </label>
+            <div className="col-sm-10">
+              <input
+                onChange={handleOnChange}
+                type="text"
+                id="apellido"
+                name="apellido"
+                value={apellido}
+                className="w-50"
+              />
+            </div>
+          </div>
+          <div className="mb-3 row">
+            <label htmlFor="email" className="col-sm-2 col-form-label">
+              Email
+            </label>
+            <div className="col-sm-10">
+              <input
+                onChange={handleOnChange}
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                className="w-50"
+              />
+            </div>
+          </div>
+          <div className="mb-3 row">
+            <label htmlFor="perfil" className="col-sm-2 col-form-label">
+              Perfil
+            </label>
+            <div className="col-sm-10">
+              <select
+                onChange={handleOnChange}
+                type="text"
+                id="perfil"
+                name="perfil"
+                value={perfil}
+                className="w-50"
+              >
+                <option value="basic">basic</option>
+                <option value="admin">admin</option>
+              </select>
+            </div>
+          </div>
+          <div className="mb-3 row">
+            <label htmlFor="password" className="col-sm-2 col-form-label">
+              Contraseña
+            </label>
+            <div className="col-sm-10">
+              <input
+                onChange={handleOnChange}
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                className="w-50"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="apellido">apellido</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            id="apellido"
-            name="apellido"
-            value={apellido}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">email</label>
-          <input
-            onChange={handleOnChange}
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-          />
-        </div>
-        <div>
-          <label htmlFor="perfil">perfil</label>
-          <select
-            onChange={handleOnChange}
-            type="text"
-            id="perfil"
-            name="perfil"
-            value={perfil}
-          >
-            <option value="basic">basic</option>
-            <option value="admin">admin</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="contraseña">contraseña</label>
-          <input
-            onChange={handleOnChange}
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-          />
-        </div>
-        <button type="submit">Registrar</button>
+        <button type="submit" className="btn btn-outline-warning text-dark mt-5">Registrar</button>
       </form>
     </>
   );
