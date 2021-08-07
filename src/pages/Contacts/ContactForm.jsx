b88cfcee37350b2ca3eeaa4803784abe079a8594
KinshinyerR <kinshinyer02@gmail.com>
2021-08-07 12:41:32 -0500

    update contact form

--- src/pages/Contacts/ContactForm.jsx
+++ src/pages/Contacts/ContactForm.jsx
@@ -48,15 +48,46 @@ const ContactForm = () => {
     channels,
   } = formData;
 
-  const [channelName, account, preference] = channels;
-
   const handleOnChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleOnChangeChannel = (e, index) => {
+    const newChannels = [...channels];
+
+    newChannels[index] = {
+      ...newChannels[index],
+      [e.target.name]: e.target.value,
+    };
+
+    setFormData({ ...formData, channels: newChannels });
+  };
+
+  const handleOnDeleteChannel = (index) => {
+    const newChannels = [...channels];
+
+    newChannels.splice(index, 1);
+
+    setFormData({ ...formData, channels: newChannels });
+  };
+
+  const handleOnAddChannel = () => {
+    setFormData({
+      ...formData,
+      channels: [
+        ...channels,
+        {
+          channelName: "",
+          account: "",
+          preference: "",
+        },
+      ],
+    });
+  };
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    // registerContact(formData).then((result) => console.log(result));
+    registerContact(formData).then((result) => console.log(result));
     console.log(formData);
   };
 
@@ -70,22 +101,22 @@ const ContactForm = () => {
   return (
     <form
       onSubmit={handleOnSubmit}
-      className="border border-warning rounded container p-5 w-50"
+      className="border border-warning rounded container p-5 "
     >
       <h1 className="mb-5">Registrar Contacto</h1>
-      <div className="ps-5 ms-5">
+      <div className="">
         <div className="mb-3 row">
           <label htmlFor="name" className="col-sm-2 col-form-label">
             Nombre
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <input
               onChange={handleOnChange}
               type="text"
               id="name"
               name="name"
               value={name}
-              className="w-50"
+              className="form-control"
             />
           </div>
         </div>
@@ -93,14 +124,14 @@ const ContactForm = () => {
           <label htmlFor="surname" className="col-sm-2 col-form-label">
             Apellido
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <input
               onChange={handleOnChange}
               type="text"
               id="surname"
               name="surname"
               value={surname}
-              className="w-50"
+              className="form-control"
             />
           </div>
         </div>
@@ -108,14 +139,14 @@ const ContactForm = () => {
           <label htmlFor="email" className="col-sm-2 col-form-label">
             Email
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <input
               onChange={handleOnChange}
               type="email"
               id="email"
               name="email"
               value={email}
-              className="w-50"
+              className="form-control"
             />
           </div>
         </div>
@@ -123,14 +154,14 @@ const ContactForm = () => {
           <label htmlFor="position" className="col-sm-2 col-form-label">
             Cargo
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <input
               onChange={handleOnChange}
               type="text"
               id="position"
               name="position"
               value={position}
-              className="w-50"
+              className="form-control"
             />
           </div>
         </div>
@@ -138,20 +169,18 @@ const ContactForm = () => {
           <label htmlFor="companyId" className="col-sm-2 col-form-label">
             Compañia
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <select
               onChange={handleOnChange}
               type="text"
               id="companyId"
               name="companyId"
               value={companyId}
-              className="w-50"
+              className="form-control"
             >
-              <option hidden selected>
-                Compañia
-              </option>
+              <option value="">---</option>
               {compamies.map((company) => (
-                <option key={company.name} value={company.id}>
+                <option key={company._id} value={company._id}>
                   {company.name}
                 </option>
               ))}
@@ -162,20 +191,18 @@ const ContactForm = () => {
           <label htmlFor="regionId" className="col-sm-2 col-form-label">
             Región
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <select
               onChange={handleOnChange}
               type="text"
               id="regionId"
               name="regionId"
               value={regionId}
-              className="w-50"
+              className="form-control"
             >
-              <option hidden selected>
-                Región
-              </option>
+              <option value="">---</option>
               {regions.map((region) => (
-                <option key={regions.name} value={regions.id}>
+                <option key={region._id} value={region._id}>
                   {region.name}
                 </option>
               ))}
@@ -186,20 +213,18 @@ const ContactForm = () => {
           <label htmlFor="countryId" className="col-sm-2 col-form-label">
             País
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <select
               onChange={handleOnChange}
               type="text"
               id="countryId"
               name="countryId"
               value={countryId}
-              className="w-50"
+              className="form-control"
             >
-              <option hidden selected>
-                País
-              </option>
+              <option value="">---</option>
               {countries.map((country) => (
-                <option key={country.name} value={country.id}>
+                <option key={country._id} value={country._id}>
                   {country.name}
                 </option>
               ))}
@@ -210,20 +235,18 @@ const ContactForm = () => {
           <label htmlFor="cityId" className="col-sm-2 col-form-label">
             Ciudad
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <select
               onChange={handleOnChange}
               type="text"
               id="cityId"
               name="cityId"
               value={cityId}
-              className="w-50"
+              className="form-control"
             >
-              <option hidden selected>
-                Ciudad
-              </option>
+              <option value="">---</option>
               {cities.map((city) => (
-                <option key={city.id} value={city.id}>
+                <option key={city._id} value={city._id}>
                   {city.name}
                 </option>
               ))}
@@ -234,14 +257,14 @@ const ContactForm = () => {
           <label htmlFor="address" className="col-sm-2 col-form-label">
             Dirección
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <input
               onChange={handleOnChange}
               type="text"
               id="address"
               name="address"
               value={address}
-              className="w-50"
+              className="form-control"
             />
           </div>
         </div>
@@ -249,14 +272,14 @@ const ContactForm = () => {
           <label htmlFor="interest" className="col-sm-2 col-form-label">
             Interés
           </label>
-          <div className="col-sm-10">
+          <div className="col-md-6">
             <input
               onChange={handleOnChange}
               type="text"
               id="interest"
               name="interest"
               value={interest}
-              className="w-50"
+              className="form-control"
             />
           </div>
         </div>
@@ -264,64 +287,78 @@ const ContactForm = () => {
           <label htmlFor="channels" className="col-sm-2 col-form-label">
             Canales
           </label>
-          <ul>
-            <li>
-              <div className="col-sm-10">
+          <button
+            type="button"
+            className="btn btn-outline-warning text-dark"
+            onClick={handleOnAddChannel}
+          >
+            Agregar Canal
+          </button>
+          {channels.map((channel, index) => (
+            <div className="my-3" key={`channel-${index}`}>
+              <div className="col-md-6">
                 <label
-                  htmlFor="channels"
+                  htmlFor={`channelName-${index}`}
                   className="col-sm-2 col-form-label me-5"
                 >
                   Nombre
                 </label>
                 <input
-                  onChange={handleOnChange}
+                  onChange={(e) => handleOnChangeChannel(e, index)}
                   type="text"
-                  id="channels"
-                  name="channels"
-                  value={channels}
-                  className="w-50"
+                  id={`channelName-${index}`}
+                  name="channelName"
+                  value={channel.channelName}
+                  className="form-control"
                 />
               </div>
-            </li>
-            <li>
-              <div className="col-sm-10">
+
+              <div className="col-md-6">
                 <label
-                  htmlFor="channels.account"
+                  htmlFor={`channelAccount-${index}`}
                   className="col-sm-2 col-form-label me-5"
                 >
                   account
                 </label>
                 <input
-                  onChange={handleOnChange}
+                  onChange={(e) => handleOnChangeChannel(e, index)}
                   type="text"
-                  id="channels.account"
-                  name="channels.account"
-                  value={channels.account}
-                  className="w-50"
+                  id={`channelAccount-${index}`}
+                  name="account"
+                  value={channel.account}
+                  className="form-control"
                 />
               </div>
-            </li>
-            <li>
-              <div className="col-sm-10">
+
+              <div className="col-md-6">
                 <label
-                  htmlFor="channels.account"
+                  htmlFor={`channelPreference-${index}`}
                   className="col-sm-2 col-form-label me-5"
                 >
                   preference
                 </label>
                 <input
-                  onChange={handleOnChange}
+                  onChange={(e) => handleOnChangeChannel(e, index)}
                   type="text"
-                  id="channels.account"
-                  name="channels.account"
-                  value={channels.account}
-                  className="w-50"
+                  id={`channelPreference-${index}`}
+                  name="preference"
+                  value={channel.preference}
+                  className="form-control"
                 />
               </div>
-            </li>
-          </ul>
+
+              <button
+                onClick={() => handleOnDeleteChannel(index)}
+                type="button"
+                className="btn btn-outline-warning text-dark mt-5"
+              >
+                Eliminar Canal
+              </button>
+            </div>
+          ))}
         </div>
       </div>
+
       <button type="submit" className="btn btn-outline-warning text-dark mt-5">
         Registrar
       </button>
