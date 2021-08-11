4788dee6887f915624652dfe03a65250dce10dde
KinshinyerR <kinshinyer02@gmail.com>
2021-08-10 20:11:35 -0500

    Add CompaniesServices

---
+++ src/lib/services/companies/companies.service.js
@@ -0,0 +1,41 @@
+export const registerCompany = (data) => {
+  const token = localStorage.getItem("token");
+  const myHeaders = new Headers();
+  myHeaders.append("x-auth-token", token);
+  myHeaders.append("Content-Type", "application/json");
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify(data),
+    redirect: "follow",
+  };
+
+  return fetch(
+    "https://data-werehouse-kr.herokuapp.com/companies/register",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+};
+
+export const getCities = () => {
+  const token = localStorage.getItem("token");
+  const myHeaders = new Headers();
+  myHeaders.append("x-auth-token", token);
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return fetch(
+    "https://data-werehouse-kr.herokuapp.com/regions/allCities",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => console.log("error", error));
+};
