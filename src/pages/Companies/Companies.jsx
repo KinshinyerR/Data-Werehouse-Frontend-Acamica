1d643451f31a6d87d543efe6f904bcdd05e3d562
KinshinyerR <kinshinyer02@gmail.com>
2021-08-12 19:24:47 -0500

    Add deleteCompany

--- src/pages/Companies/Companies.jsx
+++ src/pages/Companies/Companies.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Plantilla from "../../componentes/Plantilla/Plantilla";
+import { deleteCompany } from "../../lib/services/companies/companies.service";
 
 const Companies = () => {
   const [companies, setCompanies] = useState([]);
@@ -21,13 +22,21 @@ const Companies = () => {
       .then((response) => response.json())
       .then((result) => setCompanies(result))
       .catch((error) => console.log("error", { error }));
-  }, []);
+  }, [companies]);
 
-  const handleOnClick = (company) => {
-    console.log(company);
+  const handleOnClick = (e, company) => {
+    if (e.target.localName === "i" || e.target.localName === "button") {
+      console.log(company.email);
+      const accept = window.confirm(
+        `¿Esta seguro que desea eliminar la Compañia ${company.name}?`
+      );
+      if (accept) deleteCompany(company.email).then((result) => alert(result));
+      else {
+        alert("Compañia no eliminada");
+      }
+    }
   };
 
-  console.log(companies);
   return (
     <>
       <Plantilla title="Compañias" />
@@ -40,18 +49,27 @@ const Companies = () => {
             <th scope="col">Nombre</th>
             <th scope="col">País</th>
             <th scope="col">Dirección</th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
           {companies.length > 0 &&
             companies.map((company) => (
-              <tr key={company.email} onClick={() => handleOnClick(company)}>
+              <tr
+                key={company.email}
+                onClick={(e) => handleOnClick(e, company)}
+              >
                 <th scope="row">
                   <input type="checkbox" />
                 </th>
                 <td>{company.name}</td>
                 <td>{company.cityId.name}</td>
                 <td>{company.address}</td>
+                <td>
+                  <button className="btn btn-outline-danger">
+                    <i className="far fa-trash-alt"></i>
+                  </button>
+                </td>
               </tr>
             ))}
         </tbody>
