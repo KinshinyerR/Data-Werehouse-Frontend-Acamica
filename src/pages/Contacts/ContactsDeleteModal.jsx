ee9df72ac423dcc4859ed2f015a685cccdd6bd0d
KinshinyerR <kinshinyer02@gmail.com>
2021-10-11 19:15:19 -0500

    Delect bottons

--- src/pages/Contacts/ContactsDeleteModal.jsx
+++ src/pages/Contacts/ContactsDeleteModal.jsx
@@ -11,26 +11,26 @@ export const ContactsDeleteModal = ({ contacts, handleOnClose }) => {
   };
   return (
     <>
-      {/* // <form onSubmit={handleOnSubmit} className="container"> */}
       <div className="mb-3 row">
         <h3>¿Esta seguro que desea eliminar todos los contactos?</h3>
       </div>
 
-      <button
-        type="button"
-        onClick={handleOnSubmit}
-        className="btn btn btn-outline-success text-dark my-5 mx-0"
-      >
-        Eliminar
-      </button>
-      <button
-        type="button"
-        onClick={handleOnClose}
-        className="btn btn btn-outline-danger text-dark my-5 mx-0"
-      >
-        No Eliminar
-      </button>
-      {/* // </form> */}
+      <div className="d-flex justify-content-around">
+        <button
+          type="button"
+          onClick={handleOnSubmit}
+          className="btn btn btn-outline-success text-dark my-5 mx-0"
+        >
+          Eliminar
+        </button>
+        <button
+          type="button"
+          onClick={handleOnClose}
+          className="btn btn btn-outline-danger text-dark my-5 mx-0"
+        >
+          No Eliminar
+        </button>
+      </div>
     </>
   );
 };
