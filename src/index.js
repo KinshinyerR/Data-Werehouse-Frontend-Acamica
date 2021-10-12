f552e289a88a2eb603ac51f6022f2f49e3e3068b
KinshinyerR <kinshinyer02@gmail.com>
2021-10-12 13:03:34 -0500

    cambios de css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./app.css";
+import "../src/app.css"
 
 ReactDOM.render(
   <React.StrictMode>
